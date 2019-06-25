9167f72c50493a6bfdde987b12fd433f35d32162
ParHandler <kunilovskye@yandex.ru>
2019-06-25 09:24:25 +0300

    Unstable val playground

---
+++ src/week4/UnstableVal.kt
@@ -0,0 +1,11 @@
+package week4
+
+val foo: Int
+    get() = TODO()
+
+fun main(args: Array<String>) {
+    // The values should be different:
+    println(foo)
+    println(foo)
+    println(foo)
+}
